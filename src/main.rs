038c5893b4a8fd6821db79c24adc855839b2e4ff
yoiyoi-rat <puchipuchi1025@gmail.com>
2022-09-26 07:03:24 +0900

    can receive request and parse json, cannot send reply request

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,15 @@
-// //use actix_web::{get, post, web, HttpServer, App, HttpRequest, HttpResponse, Responder};
-use actix_web::get;
-use actix_web::post;
-use actix_web::web;
-use actix_web::HttpServer;
-use actix_web::App;
-// use actix_web::HttpRequest;
-use actix_web::HttpResponse;
-use actix_web::Responder;
-
-
+use actix_web::{middleware::Logger, web::{self, Data}, HttpServer, App, HttpResponse, Responder};
+use awc::Client;
 use dotenv::dotenv;
-use serde::Serialize;
+use serde::{Serialize, Deserialize};
 use std::env;
-// use serde::Deserialize;
-// use serde_json::Value;
 
 
+// define app state information
+#[derive(Debug, Clone)]
 struct AppInfo {
-    // channel_id_: String,
-    // channel_secret_: String,
+    channel_id_: String,
+    channel_secret_: String,
     channel_access_token_: String,
 }
 
@@ -26,15 +17,15 @@ impl AppInfo {
     pub fn new() -> AppInfo {
         dotenv().ok();
         AppInfo {
-            // channel_id_: env::var("CHANNEL_ID").unwrap(),
-            // channel_secret_: env::var("CHANNEL_SECRET").unwrap(),
+            channel_id_: env::var("CHANNEL_ID").unwrap(),
+            channel_secret_: env::var("CHANNEL_SECRET").unwrap(),
             channel_access_token_: env::var("CHANNEL_ACCESS_TOKEN").unwrap(),
         }
     }
 }
 
 
-
+// struct for json serialize to reply
 #[derive(Serialize)]
 struct Message {
     r#type: String,
@@ -42,86 +33,73 @@ struct Message {
 }
 
 #[derive(Serialize)]
-struct ResponseData {
-    reply_token_: String,
+struct LineReplyData {
+    replyToken: String,
     messages: Vec<Message>,
 }
 
 
-#[get("/webhook")]
+// handler
 async fn get_test() -> impl Responder {
     HttpResponse::Ok().body("Hello world!")
 }
 
-#[post("/webhook")]
-async fn post_test(req_body: String) -> impl Responder {
-    let appinfo = AppInfo::new();
-
+async fn post_test(req_body: String, appinfo: Data<AppInfo>) -> impl Responder {
+    // extract value from request body json
     let v:serde_json::Value = serde_json::from_str(&req_body).unwrap();
-    let replytoken = v["events"][0]["replyToken"].to_string();
-    let message_text = v["events"][0]["message"]["text"].to_string();
-    let authorization_value = "Bearer ".to_string() + appinfo.channel_access_token_.as_str();
+    let reply_token = v["events"][0]["replyToken"].as_str().unwrap().to_string();
+    let message_text = v["events"][0]["message"]["text"].as_str().unwrap().to_string();
 
-    HttpResponse::Ok()
-        .append_header(("Authorization", authorization_value))
-        .content_type("application/json")
-        //.body(message_text)
-        .json(ResponseData {
-            reply_token_: replytoken,
-            messages: vec![
-                Message {
-                    r#type: "text".to_string(),
-                    text: message_text,
-                }
-            ]
-        })
+    // make json data to reply
+    let request = serde_json::json!(LineReplyData {
+        replyToken: reply_token,
+        messages: vec![
+            Message {
+                r#type: "text".to_string(),
+                text: message_text,
+            }
+        ]
+    });
+
+    // TODO: 
+    // send post request
+    let client = Client::default();
+    let res = client
+            .post("https://api.line.me/v2/bot/message/reply") 
+            .bearer_auth(appinfo.channel_access_token_.to_string())
+            .content_type("application/json")
+            .send_json(&request)
+            .await;
+
+    // Debug
+    // println!("{}", req_body);
+    // println!("{}", request);
+    HttpResponse::Ok().body("")
 }
 
-// async fn manual_hello() -> impl Responder {
-//     HttpResponse::Ok().body("Hey there!")
-// }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    // import ans set dotenv value
+    let appinfo = Data::new(AppInfo::new());
 
-    //let appinfo = AppInfo::new();
+    // logger set
+    env_logger::init();
 
-    HttpServer::new(|| {
+    // build server
+    HttpServer::new(move || {
         App::new()
-            //.app_data(web::Data::new(appinfo))
-            .service(get_test)
-            .service(post_test)
-            //.route("/", web::get().to(manual_hello))
+        .wrap(Logger::default())
+        .app_data(Data::clone(&appinfo))
+        .route("/webhook", web::get().to(get_test))
+        .route("/webhook", web::post().to(post_test))
+        
     })
     .bind(("127.0.0.1", 8080))?
     .run()
     .await
+    
 }
 
 
 
-
-
-// #[derive(Deserialize)]
-// struct Message {
-//     id: String,
-//     message_type: String,
-//     text: String,
-// }
-
-// #[derive(Deserialize)]
-// struct Event {
-//     replyToken_: String,
-//     event_type_: String,
-//     messages_: Vec<Message>,
-// }
-
-// #[derive(Deserialize)]
-// struct Events {
-//     events_: Vec<Event>,
-// }
-
-// #[post("/webhook")]
-// pub async fn index(events: web::Json<Events>) -> impl Responder {
-//     HttpResponse::Ok().body(events) 
-// }
